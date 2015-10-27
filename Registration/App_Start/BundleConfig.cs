41caf05bfca8139a6bcd071b90b4528b06befa12
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-27 20:03:36 +0100

    Add angular.js reference to the page (_Layout.cshtml).

--- Registration/App_Start/BundleConfig.cs
+++ Registration/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace Registration
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
