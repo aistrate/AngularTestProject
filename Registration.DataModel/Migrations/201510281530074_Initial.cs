d9372531b56cbe7390c0989fee103ac1bd481403
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-28 16:32:24 +0100

    Create initial migration (tables Countries and Provinces).

---
+++ Registration.DataModel/Migrations/201510281530074_Initial.cs
@@ -0,0 +1,41 @@
+namespace Registration.DataModel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Countries",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Provinces",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CountryId = c.Int(nullable: false),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Countries", t => t.CountryId, cascadeDelete: true)
+                .Index(t => t.CountryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Provinces", "CountryId", "dbo.Countries");
+            DropIndex("dbo.Provinces", new[] { "CountryId" });
+            DropTable("dbo.Provinces");
+            DropTable("dbo.Countries");
+        }
+    }
+}
