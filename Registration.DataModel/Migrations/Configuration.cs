df83dbf04f49b6583c0d0c369ea7b65b40eb9083
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-28 18:24:25 +0100

    Add seed data (provinces).

--- Registration.DataModel/Migrations/Configuration.cs
+++ Registration.DataModel/Migrations/Configuration.cs
@@ -25,6 +25,26 @@ namespace Registration.DataModel.Migrations
 
             context.Countries.AddOrUpdate(c => c.Name, countries.ToArray());
             context.SaveChanges();
+
+            countries = context.Countries.ToList();
+
+            var provinces = new List<Province>
+            {
+                new Province { CountryId = countries.Single(c => c.Name == "Netherlands").Id, Name = "North Holland" },
+                new Province { CountryId = countries.Single(c => c.Name == "Netherlands").Id, Name = "South Holland" },
+                new Province { CountryId = countries.Single(c => c.Name == "Netherlands").Id, Name = "Limburg" },
+
+                new Province { CountryId = countries.Single(c => c.Name == "Italy").Id, Name = "Siena" },
+                new Province { CountryId = countries.Single(c => c.Name == "Italy").Id, Name = "Vicenza" },
+
+                new Province { CountryId = countries.Single(c => c.Name == "Spain").Id, Name = "Salamanca" },
+                new Province { CountryId = countries.Single(c => c.Name == "Spain").Id, Name = "Toledo" },
+                new Province { CountryId = countries.Single(c => c.Name == "Spain").Id, Name = "Albacete" },
+                new Province { CountryId = countries.Single(c => c.Name == "Spain").Id, Name = "Guadalajara" },
+            };
+
+            context.Provinces.AddOrUpdate(p => new { p.CountryId, p.Name }, provinces.ToArray());
+            context.SaveChanges();
         }
     }
 }
