2831c7b2325a46039f09130ec5fc292590cc537e
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-28 14:32:52 +0100

    Add Country and Province classes.

---
+++ Registration.Entities/Country.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registration.Entities
+{
+    public class Country
+    {
+        public Country()
+        {
+            Provinces = new List<Province>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Province> Provinces { get; set; }
+    }
+}
