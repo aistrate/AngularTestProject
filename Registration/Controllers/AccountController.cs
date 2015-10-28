094f8ab8789a0660249722181591d9e1afe0aafc
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-28 18:58:39 +0100

    Fill in the Country dropdown from the database.

--- Registration/Controllers/AccountController.cs
+++ Registration/Controllers/AccountController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Registration.DataModel;
 
 namespace Registration.Controllers
 {
     public class AccountController : Controller
     {
+        private RegistrationDbContext db = new RegistrationDbContext();
+
         public ActionResult Register()
         {
-            return View();
+            return View(db.Countries.OrderBy(c => c.Name).ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
