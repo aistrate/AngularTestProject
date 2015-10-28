94da2ab03d99e3ff66ee2abd941328beb409a8a2
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-28 15:46:49 +0100

    Create class RegistrationDbContext.

---
+++ Registration.DataModel/RegistrationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Registration.Entities;
+
+namespace Registration.DataModel
+{
+    public class RegistrationDbContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+    }
+}
