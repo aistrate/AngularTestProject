b3bfec9bddcd7cee2e61653f0a2eca469bbcba4b
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-29 02:12:05 +0100

    Fill Province dropdown on changing selection in Country dropdown.

--- Registration/Scripts/Account/Register.js
+++ Registration/Scripts/Account/Register.js
@@ -1,10 +1,37 @@
 ﻿angular.module('accountRegister', [])
-    .controller('RegisterController', function () {
+    .controller('RegisterController', function ($http) {
         var register = this;
 
         register.step = 1;
 
         register.goToStep2 = function () {
             register.step = 2;
+        };
+
+        var emptyProvinceList = function () {
+            register.provinces = [{
+                Id: 0,
+                Name: 'Please select country first'
+            }];
+            register.province = '0';
+        };
+
+        register.country = '0';
+        emptyProvinceList();
+
+        register.countryChanged = function () {
+            emptyProvinceList();
+
+            if (register.country != '0') {
+                $http.get('/api/Countries/' + register.country + '/Provinces')
+                    .then(function (response) {
+                        var emptyLine = [{
+                            Id: 0,
+                            Name: 'Select province'
+                        }];
+                        register.provinces = emptyLine.concat(response.data);
+                        register.province = '0';
+                    });
+            }
         };
     });
