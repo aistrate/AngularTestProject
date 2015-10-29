7ba65483b43fdf1f4c5e004927ae05aa179210d9
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-29 14:44:00 +0100

    Add AccountApiController (compiling, but not tested).

---
+++ Registration/Controllers/AccountApiController.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Account")]
+    public class AccountApiController : ApiController
+    {
+        public ApplicationUserManager UserManager
+        {
+            get { return Request.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(RegisterBindingModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+
+            IdentityResult result = await UserManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
