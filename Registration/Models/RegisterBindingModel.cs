570d55b41026760fba308c8de3dd3c95f1f17b59
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-30 00:15:58 +0100

    Add fields CountryId and ProvinceId to C# binding model.

--- Registration/Models/RegisterBindingModel.cs
+++ Registration/Models/RegisterBindingModel.cs
@@ -6,13 +6,17 @@ namespace Registration.Models
     public class RegisterBindingModel
     {
         [Required]
-        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+
+        [Required]
+        public int ProvinceId { get; set; }
     }
 }
