84ddce884e24564f4160a28dbd083e675da9ffd0
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-29 02:16:31 +0100

    Refactor for clarity.

--- Registration/Scripts/Account/Register.js
+++ Registration/Scripts/Account/Register.js
@@ -25,11 +25,13 @@
             if (register.country != '0') {
                 $http.get('/api/Countries/' + register.country + '/Provinces')
                     .then(function (response) {
+                        var provinces = response.data;
+
                         var emptyLine = [{
                             Id: 0,
                             Name: 'Select province'
                         }];
-                        register.provinces = emptyLine.concat(response.data);
+                        register.provinces = emptyLine.concat(provinces);
                         register.province = '0';
                     });
             }
