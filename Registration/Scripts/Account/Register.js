05fc5277b3ab59611450d072dd64fbcfeeed8efb
Adrian Istrate <adr_istrate@yahoo.com>
2015-10-29 11:10:22 +0100

    Simplify code that sets the empty line in the Province dropdown.

--- Registration/Scripts/Account/Register.js
+++ Registration/Scripts/Account/Register.js
@@ -6,31 +6,18 @@
 
         register.user = {};
 
-        var emptyProvinceList = function () {
-            register.provinces = [{
-                Id: 0,
-                Name: 'Please select country first'
-            }];
-            register.user.provinceId = '0';
-        };
-
         register.user.countryId = '0';
-        emptyProvinceList();
+        register.user.provinceId = '0';
+        register.provinces = [];
 
         register.countryChanged = function () {
-            emptyProvinceList();
+            register.user.provinceId = '0';
+            register.provinces = [];
 
             if (register.user.countryId != '0') {
                 $http.get('/api/Countries/' + register.user.countryId + '/Provinces')
                     .then(function (response) {
-                        var provinces = response.data;
-
-                        var emptyLine = [{
-                            Id: 0,
-                            Name: 'Select province'
-                        }];
-                        register.provinces = emptyLine.concat(provinces);
-                        register.user.provinceId = '0';
+                        register.provinces = response.data;
                     });
             }
         };
